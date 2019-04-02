801e4321a49e460a74852dbdd4e18aea74b4b017
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-04-03 01:22:00 +0900

    Modify css-loader for use of scss

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -33,8 +33,8 @@ module.exports = {
         loader: 'ts-loader'
       },
       {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        test: /\.(scss|css)$/,
+        loader: 'style-loader!css-loader!sass-loader'
       },
       {
         test: /\.less$/,
