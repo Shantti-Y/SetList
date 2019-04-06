bb0db079a657707eea9a4cd38bd8f16927accacc
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-04-06 22:34:09 +0900

    Remove reset.css

--- frontend/entrypoints/index.js
+++ frontend/entrypoints/index.js
@@ -5,13 +5,11 @@ import 'semantic-ui-css/semantic.min.css'
 
 import { Button } from 'react-toolbox/lib/button';
 
-import '@assets/stylesheets/reset.css';
-
 import Layout from '@layouts'
 
 import Entry from '@routes/Entry';
 import Home from '@routes/Home';
-// import About from '@routes/About';
+import About from '@routes/About';
 
 const App = () => {
   return (
@@ -19,6 +17,7 @@ const App = () => {
       <Layout>
         <Route path="/" exact component={Entry} />
         <Route path="/home" exact component={Home} />
+        <Route path="/about" exact component={About} />
       </Layout>
     </BrowserRouter>
   );
