83e3bb62b9e56ca3a62a1cf5af43033fe357d881
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-04-07 13:20:31 +0900

    Change env var in production env

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -16,8 +16,8 @@ module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.rootUrl': JSON.stringify('https://shantti-y-setlist.glitch.me'),
-      'process.env.apiUrl': JSON.stringify('https://shantti-y-setlist.glitch.me')
+      'process.env.rootUrl': JSON.stringify('https://setlify-236901.appspot.com/'),
+      'process.env.apiUrl': JSON.stringify('https://setlify-236901.appspot.com/')
     })
   ]
 });
